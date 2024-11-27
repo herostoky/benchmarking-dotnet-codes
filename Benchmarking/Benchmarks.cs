7c3323977219151258fac520f8be12270f3a97ce
Toky <72009671+herostoky@users.noreply.github.com>
2024-11-27 17:27:40 +0300

    feat: add ForAndForeach benchmark

---
+++ Benchmarking/Benchmarks.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking;
+
+[MemoryDiagnoser]
+[ShortRunJob] // <-- make it take less time 
+public class StringAndStringBuilder
+{
+    [Params(5, 50, 500)] //<-- This is a parameter attribute
+    public int N { get; set; }
+
+    [Benchmark(Baseline = true)] // <-- this is the baseline
+    public string StringJoin()
+    {
+        return string.Join(", ", Enumerable.Range(0, N).Select(i => i.ToString()));
+    }
+
+    [Benchmark(Baseline = true)]
+    public string StringBuilderI()
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < N; i++)
+        {
+            sb.Append(i);
+            sb.Append(", ");
+        }
+        return sb.ToString();
+    }
+}
+
+
+[MemoryDiagnoser]
+[ShortRunJob]
+public class ForAndForeach
+{
+    private class Person
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+    
+    [Params(5, 50, 500)]
+    public int N { get; set; }
+
+    IEnumerable<Person> GetPersons()
+    {
+        return Enumerable.Range(0, N).Select(i => new Person { Name = "P" + i, Age = i * 10 % 2  });
+    }
+
+    [Benchmark(Baseline = true)]
+    public int ForeachLoop()
+    {
+        var sumAge = 0;
+        foreach (var person in GetPersons())
+        {
+            sumAge += person.Age;
+        }
+        Console.WriteLine(sumAge);
+        return sumAge;
+    }
+
+    [Benchmark]
+    public int ForILoop()
+    {
+        var sumAge = 0;
+        for (var i = 0; i < GetPersons().Count(); i++)
+        {
+            var person = GetPersons().ElementAt(i);
+            sumAge += person.Age;
+        }
+        Console.WriteLine(sumAge);
+        return sumAge;
+    }
+}
--- Benchmarking/Program.cs
+++ Benchmarking/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+
 Console.WriteLine("Hello, World!");
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
